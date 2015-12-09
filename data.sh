448d839b76941df76909d76b6ab468e99a673ee9
Paulo Lopes <paulo@mlopes.net>
2015-12-09 20:38:06 +0100

    Initial commit

---
+++ data.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+curl -v http://localhost:8080/checkin/User-0/52.36920614146638/4.9019622802734375
+sleep 2
+curl -v http://localhost:8080/checkin/User-1/52.37035905226707/4.901618957519531
+sleep 2
+curl -v http://localhost:8080/checkin/User-2/52.368472455289265/4.879131317138672
+sleep 2
+curl -v http://localhost:8080/checkin/User-3/52.372769586703825/4.918956756591797
+sleep 2
+curl -v http://localhost:8080/checkin/User-4/52.365327947932386/4.907627105712891
+sleep 2
+curl -v http://localhost:8080/checkin/User-5/52.36302183361385/4.893035888671875
+sleep 2
+curl -v http://localhost:8080/checkin/User-6/52.36490866337327/4.912433624267577
+sleep 2
+curl -v http://localhost:8080/checkin/User-7/52.378219005717675/4.890975952148437
+sleep 2
+curl -v http://localhost:8080/checkin/User-8/52.365327947932386/4.881706237792968
+sleep 2
+curl -v http://localhost:8080/checkin/User-9/52.36144941392617/4.900245666503905
